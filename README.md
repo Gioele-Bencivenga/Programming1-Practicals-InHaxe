77340a63e0b7a601046e7dfbca96d553f0bec545
Gioele <gioelebenci@live.com>
2020-02-05 22:18:26 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Programming1-Practicals-InHaxe
+ Practical exercises done during the Programming 1 module.O
---
+++ src/Main.hx
@@ -0,0 +1,17 @@
+class Main {
+	static function main() {
+		// no byte datatype in haxe
+		// no short datatype in haxe
+
+		var x:Int = 10;
+		var y:Int = 5;
+		
+		trace("Sum between 2 Integers: " + (x + y));
+
+
+		var x1:Float = 5.7;
+		var y1:Float = 10.3;
+
+		trace("Sum between 2 Floats: " + (x1 + y1));
+	}
+}
