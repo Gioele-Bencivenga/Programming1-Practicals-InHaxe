3ecf39bc355e9e4e3b2b008fcc910a4c963907c3
Gioele <gioelebenci@live.com>
2020-02-14 13:37:45 +0000

    Started practical12

---
+++ Practical12.FinancialBob/src/Bob.hx
@@ -0,0 +1,14 @@
+class Bob{
+    var currentAge:Int;
+    var retirementAge:Int;
+    var startingSalary:Int;
+    var yearlySalaryIncreaseRate:Int; //salary increases at the end of every year
+    var monthlyMortgageAmount:Int;
+    var paidMortgage:Int;
+    var totalDueMortgage:Int;
+    var yearlyMortgageAmount:Int;
+    var monthlyUtilitiesAmount:Int;
+    var yearlyUtilitiesAmount:Int;
+    var savings:Int;
+    var taxedSalary:Int;
+}
